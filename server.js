1f7c7cd5d6ac0dc49df818bceab337814257d6eb
LAZY-CODER01 <avinashmaurya430@gmail.com>
2025-01-06 19:24:55 +0530

    cors

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+const cors = require('cors');
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://aryan-card.vercel.app'], // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you need cookies
+};
+
+app.use(cors(corsOptions));
 
 const dbconnect = require('./config/database');
 require('dotenv').config();
